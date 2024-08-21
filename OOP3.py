881b4226a3f518778101847bc4532cf2b5ad5538
Though0123 <mustafayev6111@gmail.com>
2024-08-21 15:45:04 +0500

    U

--- OOP3.py
+++ OOP3.py
@@ -13,6 +13,24 @@ class Animal:
         return(f"Каклй-то звук {self.name} и я слышу {self.voice}")
     
     def __str__(self) -> str:
-        return(f"Animal {self.name} and voice {self.voice}")
+        return(f"Animal {self.name} говорит {self.voice}")
     
-    pass
+
+class Cat(Animal):
+    def __init__(self, name, voice, color):
+        super().__init__(name, voice)
+        self.color = color
+
+    # def lissings(self):
+    #     return(f"Meow! I am {self.name} and my color is {self.color}")
+    def __str__(self) -> str:
+        return(f"Cat {self.name} and voice {self.voice} and color {self.color}")
+    
+cat=Cat("Barsik", "Meow", "Black")
+cat1=Cat("Murzik", "Meow", "White")
+cat2=Cat("Vaska", "Meow", "Gray")
+cat3=Cat("Pushok", "Meow", "Brown")
+
+print(cat1.lissings())
+print(cat2.lissings())
+print(cat3.lissings())
