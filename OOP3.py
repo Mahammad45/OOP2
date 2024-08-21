5fc3fabd909045663fbfb4a11d9552d61d4f9fd7
Though0123 <mustafayev6111@gmail.com>
2024-08-21 16:44:35 +0500

    U

--- OOP3.py
+++ OOP3.py
@@ -52,25 +52,72 @@ system('clear')
 
 
 # 2: Множественное наследования
-class A:
-    def __init__(self, name) -> None:
-        # Конструктор класса A, инициализирующий объект с атрибутом name
-        self.name = name  # Сохраняем значение name в атрибуте объекта
+# class A:
+#     def __init__(self, name) -> None:
+#         # Конструктор класса A, инициализирующий объект с атрибутом name
+#         self.name = name  # Сохраняем значение name в атрибуте объекта
 
-class B(A):
-    def __init__(self, name, age) -> None:
-        # Конструктор класса B, который наследует от класса A
-        super().__init__(name)  # Вызов конструктора родительского класса A для инициализации name
-        self.age = age  # Сохраняем значение age в атрибуте объекта
+# class B(A):
+#     def __init__(self, name, age) -> None:
+#         # Конструктор класса B, который наследует от класса A
+#         super().__init__(name)  # Вызов конструктора родительского класса A для инициализации name
+#         self.age = age  # Сохраняем значение age в атрибуте объекта
 
-class C(A):
-    def __init__(self, name, age) -> None:
-        # Конструктор класса C, который также наследует от класса A
-        super().__init__(name)  # Вызов конструктора родительского класса A для инициализации name
-        self.age = age  # Сохраняем значение age в атрибуте объекта
+# class C(A):
+#     def __init__(self, name, age) -> None:
+#         # Конструктор класса C, который также наследует от класса A
+#         super().__init__(name)  # Вызов конструктора родительского класса A для инициализации name
+#         self.age = age  # Сохраняем значение age в атрибуте объекта
 
-class D(B):
-    def __init__(self, name, age, color) -> None:
-        # Конструктор класса D, который наследует от класса B
-        super().__init__(name, age)  # Вызов конструктора родительского класса B для инициализации name и age
-        self.color = color  # Сохраняем значение color в атрибуте объекта
+# class D(B):
+#     def __init__(self, name, age, color) -> None:
+#         # Конструктор класса D, который наследует от класса B
+#         super().__init__(name, age)  # Вызов конструктора родительского класса B для инициализации name и age
+#         self.color = color  # Сохраняем значение color в атрибуте объекта
+
+
+class Account:
+    def __init__(self, first_name, last_name,phone,email) -> None:
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone = phone
+        self.email= email
+
+
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
+    
+class KaspiBank(Account):
+    def __init__(self, first_name, last_name, phone, email, __balance) -> None:
+        super().__init__(first_name, last_name, phone, email)
+        self.__balance = __balance
+
+
+    def get_balance(self):
+        return self.__balance
+
+    def __str__(self) -> str:
+        return super().__str__()
+    
+class Tele2(Account):
+    def __init__(self, first_name, last_name, phone, email, tariff) -> None:
+        super().__init__(first_name, last_name, phone, email)
+        self.tariff = tariff
+
+
+    def get_tarif(self):
+        return self.tariff
+
+    def __str__(self) -> str:
+        return super().__str__()
+    
+
+bank= KaspiBank("maga", "m", "+77776778905", "maga@example.com", 1000)
+bank1= Tele2("maga", "m", "77776778905", "maga@example.com", "premium")
+bank2= KaspiBank("maga", "m", "+77776778905", "maga@example.com", 2000)
+bank3= Tele2("maga", "m", "+77776778905", "maga@example.com", "standard")
+
+print(bank.get_balance())
+print(Tele2.get_tarif(bank1))
+print(bank2.get_balance())
+print(Tele2.get_tarif(bank3))
